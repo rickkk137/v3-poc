6e6c588196dc144e82dbfe379b7831520646282b
Dominic <dominic.lee876@gmail.com>
2025-03-20 18:14:26 -0400

    7 - Missing _baseURI() override causes empty NFT metadata

--- src/test/libraries/AlchemistNFTHelper.sol
+++ src/test/libraries/AlchemistNFTHelper.sol
@@ -1,4 +1,5 @@
-import "../../interfaces/IAlchemistV3Position.sol";
+import {IAlchemistV3Position} from "../../interfaces/IAlchemistV3Position.sol";
+import {CustomBase64} from "./CustomBase64.sol";
 
 library AlchemistNFTHelper {
     /**
@@ -15,7 +16,7 @@ library AlchemistNFTHelper {
         if (tokenCount == 0) {
             return tokenIds;
         }
-        
+
         // Loop through each token and retrieve its token ID via the enumerable interface.
         for (uint256 i = 0; i < tokenCount; i++) {
             tokenIds[i] = IAlchemistV3Position(nft).tokenOfOwnerByIndex(owner, i);
@@ -31,10 +32,58 @@ library AlchemistNFTHelper {
     function getFirstTokenId(address owner, address nft) public view returns (uint256 tokenId) {
         uint256[] memory tokenIds = getAllTokenIdsForOwner(owner, nft);
 
-        if (tokenIds.length == 0) { 
+        if (tokenIds.length == 0) {
             return 0;
         }
 
         tokenId = tokenIds[0];
+    }
+
+    /**
+     * @notice Slices a string
+     * @param text The string to slice
+     * @param start The start index
+     * @param length The length of the slice
+     * @return result The sliced string
+     */
+    function slice(string memory text, uint256 start, uint256 length) internal pure returns (string memory) {
+        bytes memory textBytes = bytes(text);
+        bytes memory result = new bytes(length);
+        for (uint256 i = 0; i < length; i++) {
+            result[i] = textBytes[start + i];
+        }
+        return string(result);
+    }
+
+    function contains(string memory source, string memory keyword) internal pure returns (bool) {
+        bytes memory sourceBytes = bytes(source);
+        bytes memory keywordBytes = bytes(keyword);
+
+        if (keywordBytes.length > sourceBytes.length) {
+            return false;
+        }
+
+        for (uint256 i = 0; i <= sourceBytes.length - keywordBytes.length; i++) {
+            bool found = true;
+            for (uint256 j = 0; j < keywordBytes.length; j++) {
+                if (sourceBytes[i + j] != keywordBytes[j]) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function base64Content(string memory uri) internal pure returns (string memory) {
+        return slice(uri, 29, bytes(uri).length - 29);
+    }
+
+    function jsonContent(string memory uri) internal pure returns (string memory) {
+        return string(CustomBase64.decode(base64Content(uri)));
     }
 }
