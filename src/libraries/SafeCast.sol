6d95804302096c2ca939fa498c8dd0393053dfd2
hesnicewithit <splitthedutch@gmail.com>
2025-03-25 10:19:45 -0400

    Earmark fix branch

--- src/libraries/SafeCast.sol
+++ src/libraries/SafeCast.sol
@@ -25,4 +25,22 @@ library SafeCast {
         }
         z = uint256(y);
     }
+
+    /// @notice Cast a uint256 to a uint128, revert on underflow
+    /// @param y The uint256 to be casted
+    /// @return z The casted integer, now type uint128
+    function uint256ToUint128(uint256 y) internal pure returns (uint128 z) {
+        if (y > type(uint128).max) {
+            revert IllegalArgument();
+        }
+        z = uint128(y);
+    }
+
+    /// @notice Cast a uint128 to a uint256
+    /// @param y The uint128 to be casted
+    /// @return z The casted integer, now type uint256
+    function uint128ToUint256(uint128 y) internal pure returns (uint256 z) {
+        // Upcast will not overflow
+        z = uint256(y);
+    }
 }
