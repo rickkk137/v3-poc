cf5cac34da641a27603eaf95290f7a2be55df590
silur <3187667+Silur@users.noreply.github.com>
2025-07-22 15:19:47 +0400

    use solidity 0.8.28

--- src/test/libraries/AlchemistNFTHelper.sol
+++ src/test/libraries/AlchemistNFTHelper.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
+pragma solidity 0.8.28;
 
 import {IAlchemistV3Position} from "../../interfaces/IAlchemistV3Position.sol";
 import {CustomBase64} from "./CustomBase64.sol";
